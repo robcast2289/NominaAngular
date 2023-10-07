353e769d0660ec3249a2cb612009645ebd707a1a
Roberto <tonocastrom@gmail.com>
2023-10-07 03:08:09 -0600

    publish

--- src/app/config/urlserver.config.ts
+++ src/app/config/urlserver.config.ts
@@ -3,7 +3,7 @@ import { environment } from '../../environments/environment';
 let URL_SERVER: string;
 // Servidor
 if (environment.production) {
-    URL_SERVER = 'http://34.71.87.248:80/'; 
+    URL_SERVER = 'http://34.31.15.103:80/'; 
 } else {
     URL_SERVER = 'http://localhost:8080/';
     //URL_SERVER = 'http://34.71.87.248:80/'; 
